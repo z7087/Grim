9c4ff64644698c01151655acdeac66737c420cf8
z7087 <75190047+z7087@users.noreply.github.com>
2023-10-30 16:44:20 +0800

    allalalallalalalllal

--- src/main/java/ac/grim/grimac/checks/impl/combat/UseHit.java
+++ src/main/java/ac/grim/grimac/checks/impl/combat/UseHit.java
@@ -4,6 +4,7 @@ import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
+import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
