b872c9650d3fb03836ebc7d749413c52985657e3
z7087 <75190047+z7087@users.noreply.github.com>
2023-11-14 08:46:57 +0800

    更新 RotationPlace.java

--- src/main/java/ac/grim/grimac/checks/impl/scaffolding/RotationPlace.java
+++ src/main/java/ac/grim/grimac/checks/impl/scaffolding/RotationPlace.java
@@ -14,6 +14,7 @@ import com.github.retrooper.packetevents.protocol.world.BlockFace;
 import com.github.retrooper.packetevents.protocol.world.states.type.StateTypes;
 import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.util.Vector3f;
+import com.github.retrooper.packetevents.util.Vector3i;
 import org.bukkit.util.Vector;
 
 import java.util.ArrayList;
@@ -23,23 +24,10 @@ import java.util.List;
 
 @CheckData(name = "RotationPlace")
 public class RotationPlace extends BlockPlaceCheck {
-    double flagBuffer = 0; // If the player flags once, force them to play legit, or we will cancel the tick before.
-    boolean ignorePost = false;
+    double threshold = 0.0001;
 
     public RotationPlace(GrimPlayer player) {
         super(player);
-    }
-
-    @Override
-    public void onBlockPlace(final BlockPlace place) {
-        if (place.getMaterial() == StateTypes.SCAFFOLDING) return;
-        if (flagBuffer > 0 && !didRayTraceHit(place)) {
-            ignorePost = true;
-            // If the player hit and has flagged this check recently
-            if (flagAndAlert("pre-flying") && shouldModifyPackets() && shouldCancel()) {
-                place.resync();  // Deny the block placement.
-            }
-        }
     }
 
     // Use post flying because it has the correct rotation, and can't false easily.
@@ -47,43 +35,40 @@ public class RotationPlace extends BlockPlaceCheck {
     public void onPostFlyingBlockPlace(PostBlockPlace place) {
         if (place.getMaterial() == StateTypes.SCAFFOLDING) return;
 
-        // Don't flag twice
-        if (ignorePost) {
-            ignorePost = false;
-            return;
-        }
-
-        // Ray trace to try and hit the target block.
-        boolean hit = didRayTraceHit(place);
         // This can false with rapidly moving yaw in 1.8+ clients
-        if (!hit) {
-            flagBuffer = 1;
+        if (!didRayTraceHit(place)) {
             flagAndAlert("post-flying");
-        } else {
-            flagBuffer = Math.max(0, flagBuffer - 0.1);
         }
     }
 
-    private boolean didRayTraceHit(BlockPlace place) {
-        SimpleCollisionBox box = new SimpleCollisionBox(place.getPlacedAgainstBlockLocation());
+    private boolean didRayTraceHit(PostBlockPlace place) {
+        Vector3i placeLocation = place.getPlacedAgainstBlockLocation();
+        Vector3f cursor = place.getCursor();
+        Vector3d clickLocation = new Vector3d(placeLocation.getX() + cursor.getX(), placeLocation.getY() + cursor.getY(), placeLocation.getZ() + cursor.getZ());
 
-        List<Vector3f> possibleLookDirs = new ArrayList<>(Arrays.asList(
-                new Vector3f(player.lastXRot, player.yRot, 0),
-                new Vector3f(player.xRot, player.yRot, 0)
-        ));
+        SimpleCollisionBox box = new SimpleCollisionBox(clickLocation, clickLocation).expand(threshold);
+        box.expand(player.getMovementThreshold());
 
         // Start checking if player is in the block
         double minEyeHeight = Collections.min(player.getPossibleEyeHeights());
         double maxEyeHeight = Collections.max(player.getPossibleEyeHeights());
 
         SimpleCollisionBox eyePositions = new SimpleCollisionBox(player.x, player.y + minEyeHeight, player.z, player.x, player.y + maxEyeHeight, player.z);
-        box.expand(player.getMovementThreshold());
 
         // If the player is inside a block, then they can ray trace through the block and hit the other side of the block
         if (eyePositions.isIntersected(box)) {
             return true;
         }
         // End checking if the player is in the block
+
+
+        float yaw = place.getHasRotation() ? place.getYaw() : player.xRot;
+        float pitch = place.getHasRotation() ? place.getPitch() : player.yRot;
+
+        List<Vector3f> possibleLookDirs = new ArrayList<>(Arrays.asList(
+                new Vector3f(player.lastXRot, pitch, 0),
+                new Vector3f(yaw, pitch, 0)
+        ));
 
         // 1.9+ players could be a tick behind because we don't get skipped ticks
         if (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9)) {
@@ -92,7 +77,7 @@ public class RotationPlace extends BlockPlaceCheck {
 
         // 1.7 players do not have any of these issues! They are always on the latest look vector
         if (player.getClientVersion().isOlderThan(ClientVersion.V_1_8)) {
-            possibleLookDirs = Collections.singletonList(new Vector3f(player.xRot, player.yRot, 0));
+            possibleLookDirs = Collections.singletonList(new Vector3f(yaw, pitch, 0));
         }
 
         for (double d : player.getPossibleEyeHeights()) {
