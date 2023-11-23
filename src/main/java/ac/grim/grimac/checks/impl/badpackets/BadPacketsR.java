fd44473677164539f94ac6500f6ed89e7034a491
z7087 <75190047+z7087@users.noreply.github.com>
2023-11-23 19:03:14 +0800

    rubbish

--- src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsR.java
+++ src/main/java/ac/grim/grimac/checks/impl/badpackets/BadPacketsR.java
@@ -2,11 +2,16 @@ package ac.grim.grimac.checks.impl.badpackets;
 
 import ac.grim.grimac.checks.Check;
 import ac.grim.grimac.checks.CheckData;
+import ac.grim.grimac.checks.impl.post.PostCheck;
 import ac.grim.grimac.checks.type.PacketCheck;
 import ac.grim.grimac.player.GrimPlayer;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.protocol.player.GameMode;
+
+enum Status {
+    outTick, beforeTick, afterTick;
+}
 
 @CheckData(name = "BadPacketsR", decay = 0.1)
 public class BadPacketsR extends Check implements PacketCheck {
@@ -14,36 +19,44 @@ public class BadPacketsR extends Check implements PacketCheck {
         super(player);
     }
 
-    private int positions = 0;
-    private long clock = 0;
-    private long lastTransTime;
+    private boolean hadClientRunTick = false;
+    private long lastTransTime = -1;
+    private Status clientStatus = Status.outTick;
     private int oldTransId = 0;
 
     @Override
     public void onPacketReceive(final PacketReceiveEvent event) {
         if (isTransaction(event.getPacketType()) && player.packetStateData.lastTransactionPacketWasValid) {
-            long ms = (player.getPlayerClockAtLeast() - clock) / 1000000L;
-            long diff = (System.currentTimeMillis() - lastTransTime);
-            if (diff > 2000 && ms > 2000) {
-                //TODO: figure out why spectators are flagging this
-                if (positions == 0 && clock != 0 && player.gamemode != GameMode.SPECTATOR) {
-                    flagAndAlert("time=" + ms + "ms, " + "lst=" + diff + "ms, positions=" + positions);
-                } else {
-                    reward();
-                }
-                player.compensatedWorld.removeInvalidPistonLikeStuff(oldTransId);
-                positions = 0;
-                clock = player.getPlayerClockAtLeast();
+            // client ran some ticks and send a transaction
+            if (clientStatus != Status.outTick) {
                 lastTransTime = System.currentTimeMillis();
                 oldTransId = player.lastTransactionSent.get();
+                shouldCheckNextTick = true;
+                hadClientRunTick = false;
             }
-        }
-        //
-        if ((event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION ||
+            clientStatus = Status.outTick;
+        } else if (player.checkManager.getPostPredictionCheck(PostCheck.class).shouldCountPacketForPost(event.getPacketType())) {
+            if (shouldCheckNextTick) {
+                long diff = (System.currentTimeMillis() - lastTransTime);
+                if (diff > 1000) {
+                    //TODO: figure out why spectators are flagging this
+                    if (lastTransTime != -1 && hadClientRunTick && player.gamemode != GameMode.SPECTATOR) {
+                        flagAndAlert("lst=" + diff + "ms");
+                    } else {
+                        reward();
+                    }
+                    player.compensatedWorld.removeInvalidPistonLikeStuff(oldTransId);
+                }
+            }
+            hadClientRunTick = true;
+            clientStatus = Status.beforeTick;
+        } else if ((event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION_AND_ROTATION ||
                 event.getPacketType() == PacketType.Play.Client.PLAYER_POSITION) && !player.compensatedEntities.getSelf().inVehicle()) {
-            positions++;
+            hadClientRunTick = true;
+            clientStatus = Status.afterTick;
         } else if (event.getPacketType() == PacketType.Play.Client.STEER_VEHICLE && player.compensatedEntities.getSelf().inVehicle()) {
-            positions++;
+            hadClientRunTick = true;
+            clientStatus = Status.afterTick;
         }
     }
 
