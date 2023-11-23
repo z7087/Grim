42537c5241afd2b7a9430eccf4dc5b539382d8c3
z7087 <75190047+z7087@users.noreply.github.com>
2023-11-23 18:27:29 +0800

    Update PostCheck.java

--- src/main/java/ac/grim/grimac/checks/impl/post/PostCheck.java
+++ src/main/java/ac/grim/grimac/checks/impl/post/PostCheck.java
@@ -85,28 +85,35 @@ public class PostCheck extends Check implements PacketCheck, PostPredictionCheck
                 }
                 post.clear();
                 sentFlying = false;
-            } else if (PLAYER_ABILITIES.equals(packetType)
-                    || INTERACT_ENTITY.equals(packetType) || PLAYER_BLOCK_PLACEMENT.equals(packetType)
-                    || USE_ITEM.equals(packetType) || PLAYER_DIGGING.equals(packetType)) {
-                if (sentFlying) post.add(event.getPacketType());
-            } else if (CLICK_WINDOW.equals(packetType) && player.getClientVersion().isOlderThan(ClientVersion.V_1_13)) {
-                // Why do 1.13+ players send the click window packet whenever? This doesn't make sense.
-                if (sentFlying) post.add(event.getPacketType());
-            } else if (ANIMATION.equals(packetType)
-                    && (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9) // ViaVersion delays animations for 1.8 clients
-                    || PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_8_8)) // when on 1.9+ servers
-                    && player.getClientVersion().isOlderThan(ClientVersion.V_1_13) // 1.13 clicking inventory causes weird animations
-                    && isExemptFromSwingingCheck < player.lastTransactionReceived.get()) { // Exempt when the server sends animations because viaversion
-                if (sentFlying) post.add(event.getPacketType());
-            } else if (ENTITY_ACTION.equals(packetType) // ViaRewind sends START_FALL_FLYING packets async for 1.8 clients on 1.9+ servers
-                    && ((player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9) // ViaRewind doesn't 1.9 players
-                    || PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_8_8)))) { // No elytras
-                // https://github.com/GrimAnticheat/Grim/issues/824
-                if (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_19_3) && player.compensatedEntities.getSelf().getRiding() != null) {
-                    return;
-                }
+            } else if (shouldCountPacketForPost(packetType)) {
                 if (sentFlying) post.add(event.getPacketType());
             }
         }
     }
+
+    public boolean shouldCountPacketForPost(final PacketTypeCommon packetType) {
+        if (PLAYER_ABILITIES.equals(packetType)
+                || INTERACT_ENTITY.equals(packetType) || PLAYER_BLOCK_PLACEMENT.equals(packetType)
+                || USE_ITEM.equals(packetType) || PLAYER_DIGGING.equals(packetType)) {
+            return true;
+        } else if (CLICK_WINDOW.equals(packetType) && player.getClientVersion().isOlderThan(ClientVersion.V_1_13)) {
+            // Why do 1.13+ players send the click window packet whenever? This doesn't make sense.
+            return true;
+        } else if (ANIMATION.equals(packetType)
+                && (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9) // ViaVersion delays animations for 1.8 clients
+                || PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_8_8)) // when on 1.9+ servers
+                && player.getClientVersion().isOlderThan(ClientVersion.V_1_13) // 1.13 clicking inventory causes weird animations
+                && isExemptFromSwingingCheck < player.lastTransactionReceived.get()) { // Exempt when the server sends animations because viaversion
+            return true;
+        } else if (ENTITY_ACTION.equals(packetType) // ViaRewind sends START_FALL_FLYING packets async for 1.8 clients on 1.9+ servers
+                && ((player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_9) // ViaRewind doesn't 1.9 players
+                || PacketEvents.getAPI().getServerManager().getVersion().isOlderThanOrEquals(ServerVersion.V_1_8_8)))) { // No elytras
+            // https://github.com/GrimAnticheat/Grim/issues/824
+            if (player.getClientVersion().isNewerThanOrEquals(ClientVersion.V_1_19_3) && player.compensatedEntities.getSelf().getRiding() != null) {
+                return false;
+            }
+            return true;
+        }
+    }
+
 }
